3024816b5b23bfe6fc87d38f4fbbc930d0051472
kyawe1 <kk09254010314@gmail.com>
2021-12-29 11:52:13 +0630

    joker

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,19 @@
     Login
 @endsection
 @section('body')
-    <div class='container d-block m-auto w-75'>
+    <div class='container d-block m-auto w-75 p-2'>
         <form method='post' class='d-block m-auto w-50 lh-base'>
             @csrf
             <div>
-                <label for='username'>UserName</label>
-                <input type='text' id='username' name='name' placeholder="username">
+                <label for='username' class="form-label">UserName</label>
+                <input type='text' id='username' name='name' placeholder="username" class='form-control'>
             </div>
             <div>
-                <label for='Password'>Password</label>
-                <input type='password' id='Password' name='password' placeholder="Password">
+                <label for='Password' class="form-label">Password</label>
+                <input type='password' id='Password' name='password' placeholder="Password" class='form-control'>
             </div>
             <div>
-                <input type='submit' class='btn btn-primary w-75'>
+                <input type='submit' class='btn btn-primary w-75' value="login">
             </div>
         </form>
     </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,23 +7,23 @@
         <form method='post'>
             @csrf
             <div class='d-block m-auto'>
-                <label for='username'>UserName</label>
-                <input type='text' id='username' name='name' placeholder="username">
+                <label for='username' class='form-label'>UserName</label>
+                <input type='text' id='username' name='name' placeholder="username" class='form-control'>
             </div>
             <div class='d-block m-auto'>
-                <label for='Email'>Email</label>
-                <input type='email' id='Email' name='email' placeholder="Email">
+                <label for='Email' class='form-label'>Email</label>
+                <input type='email' id='Email' name='email' placeholder="Email" class='form-control'>
             </div>
             <div class='d-block m-auto'>
-                <label for='Password'>Password</label>
-                <input type='password' id='Password' name='password' placeholder="Password">
+                <label for='Password' class='form-label'>Password</label>
+                <input type='password' id='Password' name='password' placeholder="Password" class='form-control'>
             </div>
             <div class='d-block m-auto'>
-                <label for='RePassword'>Confirm Password</label>
-                <input type='password' id='RePassword' name='repassword' placeholder="Confirm Password">
+                <label for='RePassword' class='form-label'>Confirm Password</label>
+                <input type='password' id='RePassword' name='repassword' placeholder="Confirm Password" class='form-control'>
             </div>
             <div class='d-block m-auto'>
-                <input type='submit' class='btn btn-primary w-50'>
+                <input type='submit' class='btn btn-primary w-50' value='register'>
             </div>
         </form>
     </div>
